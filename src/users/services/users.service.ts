a205697b3b3ef2d14d42035c009e54f0e2c0705d
Leonardo Gonzalez <10843115+ingleo@users.noreply.github.com>
2023-09-03 16:41:16 -0500

    added user module

---
+++ src/users/services/users.service.ts
@@ -0,0 +1,58 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { User } from '../entities/user.entity';
+import { CreateUserDto, UpdateUserDto } from '../dtos/user.dto';
+
+@Injectable()
+export class UsersService {
+  private counterId = 1;
+  private users: User[] = [
+    {
+      id: 1,
+      email: 'correo@gmail.com',
+      password: '12345',
+      role: 'admin',
+    },
+  ];
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundException(`User ${id} not found`);
+    }
+    return user;
+  }
+
+  create(payload: CreateUserDto) {
+    this.counterId = this.counterId + 1;
+    const newUser = {
+      id: this.counterId,
+      ...payload,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(id: number, payload: UpdateUserDto) {
+    const user = this.findOne(id);
+    const index = this.users.findIndex((user) => user.id === id);
+    this.users[index] = {
+      ...user,
+      ...payload,
+    };
+    return this.users[index];
+  }
+
+  remove(id: number) {
+    const index = this.users.findIndex((user) => user.id === id);
+    if (index === -1) {
+      throw new NotFoundException(`User ${id} not found`);
+    }
+    this.users.splice(index, 1);
+    return true;
+  }
+}
