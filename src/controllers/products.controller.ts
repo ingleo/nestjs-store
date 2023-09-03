2f055be9350c11a28ea63440c9921c9ef97a35bf
Leonardo Gonzalez <10843115+ingleo@users.noreply.github.com>
2023-09-02 23:06:03 -0500

    first commit

---
+++ src/controllers/products.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ProductsService } from '../services/products.service';
+import { CreateProductDto, UpdateProductDto } from '../dtos/product.dto';
+import { ParseIntPipe } from '../common/parse-int/parse-int.pipe';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+  @Get()
+  getProducts() {
+    return this.productsService.findAll();
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.ACCEPTED)
+  getOne(@Param('id', ParseIntPipe) productId: number) {
+    return this.productsService.findOne(productId);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() payload: CreateProductDto) {
+    return this.productsService.create(payload);
+  }
+
+  @Put('/:id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateProductDto,
+  ) {
+    return this.productsService.update(id, payload);
+  }
+
+  @Delete('/:id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.remove(id);
+  }
+}
